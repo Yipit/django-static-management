1043ab06d74e7695a722b60f5ad821bb8eb8f1bf
Zach Smith <zmsmith27@gmail.com>
2010-09-13 13:25:24 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#/usr/bin/env python
+from setuptools import setup, find_packages
+ 
+setup(
+    name='django-static-management',
+    version='1.2',
+    description='Compression tool for static CSS and JS files',
+    author='Justin Driscoll',
+    author_email='justin@driscolldev.com',
+    url='http://github.com/Yipit/django-static-management/',
+)
