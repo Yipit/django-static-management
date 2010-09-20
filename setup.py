62739016bd3a014416e89d8f37e5d98a29efad62
zmsmith <zmsmith27@gmail.com>
2010-09-20 15:00:28 -0400

    v1.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-static-management',
-    version='1.2.1',
+    version='1.3.0',
     description='Compression tool for static CSS and JS files',
     author='Justin Driscoll',
     author_email='justin@driscolldev.com',
