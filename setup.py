b23a24dbff7cbb2d5cd5269c1d976b426e3b296e
Ben Plesser <ben@yipit.com>
2012-07-02 17:16:13 -0300

    Bumpin' that thang

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-static-management',
-    version='1.5.8',
+    version='1.5.9',
     description='Compression tool for static CSS and JS files',
     author='Zach Smith',
     author_email='zmsmith27@gmail.com',
