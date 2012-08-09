5fcd07baafc79d8bd4f130ddcd9bf9f0ff4bd006
zmsmith <zmsmith27@gmail.com>
2012-08-09 12:02:31 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
- 
+
 setup(
     name='django-static-management',
-    version='1.5.9',
+    version='1.5.10',
     description='Compression tool for static CSS and JS files',
     author='Zach Smith',
     author_email='zmsmith27@gmail.com',
