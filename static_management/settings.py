6373bee7e8627c26dc8dbe86544419efbcd1199b
zmsmith <zmsmith27@gmail.com>
2012-08-09 12:00:13 -0400

    add new setting to serve compressed on debug

--- static_management/settings.py
+++ static_management/settings.py
@@ -5,9 +5,9 @@ from django.conf import settings
 STATIC_MANAGEMENT_COMPRESS_CMD = getattr(settings, 'STATIC_MANAGEMENT_COMPRESS_CMD', '')
 STATIC_MANAGEMENT_CSS_LINK = getattr(settings, 'STATIC_MANAGEMENT_CSS_LINK', '')
 STATIC_MANAGEMENT_SCRIPT_SRC = getattr(settings, 'STATIC_MANAGEMENT_SCRIPT_SRC', '<script type="text/javascript" src="%s"></script>\n')
-STATIC_MANAGEMENT_VERSIONER = getattr(settings, 'STATIC_MANAGEMENT_VERSIONER','static_management.versioners.SHA1Sum')
-STATIC_MANAGEMENT = getattr(settings, 'STATIC_MANAGEMENT',{})
-STATIC_MANAGEMENT_USE_VERSIONS = getattr(settings, 'STATIC_MANAGEMENT_USE_VERSIONS',True)
+STATIC_MANAGEMENT_VERSIONER = getattr(settings, 'STATIC_MANAGEMENT_VERSIONER', 'static_management.versioners.SHA1Sum')
+STATIC_MANAGEMENT = getattr(settings, 'STATIC_MANAGEMENT', {})
+STATIC_MANAGEMENT_USE_VERSIONS = getattr(settings, 'STATIC_MANAGEMENT_USE_VERSIONS', True)
 STATIC_MANAGEMENT_SYNC_COMMAND = getattr(settings, 'STATIC_MANAGEMENT_SYNC_COMMAND', '')
 STATIC_MANAGEMENT_SYNC_COMMAND_KWARGS = getattr(settings, 'STATIC_MANAGEMENT_SYNC_COMMAND_KWARGS', {})
 
@@ -27,3 +27,4 @@ CSS_MEDIA_URL = getattr(settings, 'CSS_MEDIA_URL') or MEDIA_URL
 JS_MEDIA_URL = getattr(settings, 'JS_MEDIA_URL') or MEDIA_URL
 MEDIA_ROOT = getattr(settings, 'MEDIA_ROOT')
 DEBUG = getattr(settings, 'DEBUG')
+SERVE_COMPRESSED = getattr(settings, 'STATIC_MANAGEMENT_SERVE_COMPRESSED', (not DEBUG))
--- static_management/templatetags/static_combo.py
+++ static_management/templatetags/static_combo.py
@@ -1,5 +1,4 @@
 import os
-import time
 
 from django import template
 register = template.Library()
@@ -24,8 +23,9 @@ for inheritance_key in SUPPORTED_FILE_TYPES:
                 continue
             except FileVersion.DoesNotExist:
                 pass
-        
+
         FILE_VERSIONS[inheritance_key][file_key] = file_key
+
 
 @register.simple_tag(takes_context=True)
 def static_combo_css(context, file_name, media=None):
@@ -39,18 +39,20 @@ def static_combo_css(context, file_name, media=None):
             link_format = '<link rel="stylesheet" type="text/css" href="%%s" media="%s">\n' % media
         else:
             link_format = '<link rel="stylesheet" type="text/css" href="%s">\n'
-    gzipped = 'gzip' in context['request'].META.get('HTTP_ACCEPT_ENCODING', " ")
+    gzipped = ('gzip' in context['request'].META.get('HTTP_ACCEPT_ENCODING', " ") and not settings.DEBUG)
     output = _group_file_names_and_output(file_name, link_format, 'css', gzipped, media_url=settings.CSS_MEDIA_URL)
     return output
+
 
 @register.simple_tag(takes_context=True)
 def static_combo_js(context, file_name):
     """combines files in settings
     {% static_combo_js "js/main.js" %}"""
     script_format = settings.STATIC_MANAGEMENT_SCRIPT_SRC
-    gzipped = 'gzip' in context['request'].META.get('HTTP_ACCEPT_ENCODING', " ")
+    gzipped = ('gzip' in context['request'].META.get('HTTP_ACCEPT_ENCODING', " ") and not settings.DEBUG)
     output = _group_file_names_and_output(file_name, script_format, 'js', gzipped, media_url=settings.JS_MEDIA_URL)
     return output
+
 
 def _group_file_names_and_output(parent_name, output_format, inheritance_key, gzipped=False, media_url=settings.MEDIA_URL):
     """helper function to do most of the heavy lifting of the above template tags"""
@@ -59,7 +61,7 @@ def _group_file_names_and_output(parent_name, output_format, inheritance_key, gz
     except AttributeError:
         raise template.TemplateSyntaxError, "%s not in static combo settings" % parent_name
     output = ''
-    if settings.DEBUG:
+    if not settings.SERVE_COMPRESSED:
         # we need to echo out each one
         for file_name in file_names:
             file_path = os.path.join(settings.MEDIA_ROOT, file_name)
@@ -75,6 +77,6 @@ def _group_file_names_and_output(parent_name, output_format, inheritance_key, gz
     else:
         filename = FILE_VERSIONS[inheritance_key][parent_name]
         if gzipped:
-            filename = "%s.jgz" % filename # Old versions of Safari have issues with .gz, so now switching to .jgz
+            filename = "%s.jgz" % filename  # Old versions of Safari have issues with .gz, so now switching to .jgz
         output = output_format % os.path.join(media_url, filename)
     return output
