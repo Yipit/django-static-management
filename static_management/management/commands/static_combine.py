e9d066e8363877bb11016b6d4894d8dcf1b5842d
zmsmith <zmsmith27@gmail.com>
2012-08-31 15:24:45 -0400

    fix static management

--- static_management/management/commands/static_combine.py
+++ static_management/management/commands/static_combine.py
@@ -14,21 +14,22 @@ from static_management.models import FileVersion, FileModified
 import logging
 logger = logging.getLogger(settings.STATIC_MANAGEMENT_LOGGER)
 
+
 class Command(BaseCommand):
     """static management commands for static_combine argument"""
-    
+
     option_list = BaseCommand.option_list + (
         make_option("-c", "--no-compress", action="store_false", dest="compress", default=True, help='Runs the compression script defined in "STATIC_MANAGEMENT_COMPRESS_CMD" on the final combined files'),
         make_option("-s", "--no-sync", action="store_false", dest="sync", default=True, help='Outputs the list of filenames with version info using the "STATIC_MANAGEMENT_VERSION_OUTPUT"'),
     )
-    
+
     def handle(self, *args, **kwargs):
         self.options = kwargs
         self.combine_js()
         self.combine_css()
         if self.options['sync'] and settings.STATIC_MANAGEMENT_SYNC_COMMAND:
             call_command(settings.STATIC_MANAGEMENT_SYNC_COMMAND, **settings.STATIC_MANAGEMENT_SYNC_COMMAND_KWARGS)
-        
+
     def combine_js(self):
         logger.info("Combining js....")
         try:
@@ -38,7 +39,7 @@ class Command(BaseCommand):
             js_files = None
         if js_files:
             combine_files(js_files, self.options)
-    
+
     def combine_css(self):
         logger.info("Combining css....")
         try:
@@ -49,46 +50,47 @@ class Command(BaseCommand):
         if css_files:
             combine_files(css_files, self.options)
 
+
 def combine_files(files, options):
     for main_file in files:
-        if not is_modified(files[main_file]):
+        contained_files = files[main_file]
+        if not is_modified(main_file, contained_files):
             logging.info("Skipping static_combine for files under %s since none of them have been modified", main_file)
             continue
-        to_combine = recurse_files(main_file, files[main_file], files)
+        to_combine = recurse_files(main_file, contained_files, files)
         to_combine_paths = [os.path.join(settings.MEDIA_ROOT, f_name) for f_name in to_combine if os.path.exists(os.path.join(settings.MEDIA_ROOT, f_name))]
         logging.info("Building %s" % main_file)
         static_combine(main_file, to_combine_paths, compress=options['compress'])
 
-def is_modified(files):
-    is_modified = False
+
+def is_modified(file_key, files):
+    file_version, created = FileVersion.objects.get_or_create(file_key=file_key)
+    if created:
+        return True
+
+    db_file_ts = file_version.datetime
     for the_file in files:
-        db_file, created = FileModified.objects.get_or_create(filename=the_file)
-        if created:
-            is_modified = True
-        else:
-            db_file_ts = db_file.last_modified
-            path_to_file = os.path.join(settings.MEDIA_ROOT, db_file.filename)
-            if os.path.isfile(path_to_file):
-                system_file_ts = datetime.datetime.fromtimestamp(os.path.getmtime(path_to_file))
-                if system_file_ts > db_file_ts:
-                    db_file.last_modified = system_file_ts
-                    db_file.save()
-                    is_modified = True
-    return is_modified
+        path_to_file = os.path.join(settings.MEDIA_ROOT, the_file)
+        if os.path.isfile(path_to_file):
+            system_file_ts = datetime.datetime.fromtimestamp(os.path.getmtime(path_to_file))
+            if system_file_ts > db_file_ts:
+                return True
+    return False
+
 
 def recurse_files(name, files, top):
     """
     given following format:
-    
+
     {
         "filename": ["file1", "file2", "file3"],
         "filename2": ["filename", "file4"]
     }
-    
+
     name="filename"
     files=["file1", "file2", "file3"]
     top = Whole dictionary
-    
+
     if a value on the left appears on the right, inherit those files
     """
     combine_files = []
@@ -105,11 +107,8 @@ def static_combine(end_file_key, to_combine, delimiter="\n/* Begin: %s */\n", co
 
     Usage: static_combine(my_ultimate_file, list_of_paths, [delimiter])
 
-    delimiter is set to a Javascript and CSS safe comment to note where files 
+    delimiter is set to a Javascript and CSS safe comment to note where files
     start"""
-    # FIXME this fails in the face of @import directives in the CSS.
-    # a) we need to move all remote @imports up to the top
-    # b) we need to recursively expand all local @imports
     end_file = os.path.join(settings.MEDIA_ROOT, end_file_key)
     combo_file = open(end_file, 'w')
     to_write = ''
@@ -127,10 +126,10 @@ def static_combine(end_file_key, to_combine, delimiter="\n/* Begin: %s */\n", co
         combo_file.close()
         if compress:
             try:
-                command =  settings.STATIC_MANAGEMENT_COMPRESS_CMD % end_file
-            except AttributeError, error:
+                command = settings.STATIC_MANAGEMENT_COMPRESS_CMD % end_file
+            except AttributeError:
                 raise CommandError("STATIC_MANAGEMENT_COMPRESS_CMD not set")
-            except TypeError, error:
+            except TypeError:
                 raise CommandError("No string substitution provided for the input file to be passed to the argument ('cmd %s')")
             proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
             to_write = proc.communicate()[0]
@@ -148,4 +147,3 @@ def static_combine(end_file_key, to_combine, delimiter="\n/* Begin: %s */\n", co
             combo_file = open(end_file, 'w')
             combo_file.write(to_write)
             combo_file.close()
-        
