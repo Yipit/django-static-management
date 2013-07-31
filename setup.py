f683e967c202507c1180d8db186cb2628f0ed07b
Steve Pulec <spulec@gmail.com>
2013-07-31 16:24:51 -0400

    Renaming distribution to be Yipit specific

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-static-management',
+    name='django-static-management-yipit',
     version='1.5.13',
     description='Compression tool for static CSS and JS files',
     author='Zach Smith',
